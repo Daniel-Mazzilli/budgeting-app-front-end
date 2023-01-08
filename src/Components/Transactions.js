08e71c3ec256c6a1b8bf4cfa29cfcdbf1920a159
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-08 01:29:11 -0500

    setting up index page

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
+
 export default function Transactions() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/transactions`)
+      .then((res) => setTransactions(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      <p>transactions go here</p>
+      <h3>Index</h3>
+      <ul>
+        {transactions &&
+          transactions.map((transaction) => {
+            return <li key={transaction.id}>{transaction.item_name}</li>;
+          })}
+      </ul>
     </div>
   );
 }
