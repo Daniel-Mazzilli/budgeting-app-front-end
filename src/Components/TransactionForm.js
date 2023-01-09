520b8816ee91f31b95d7dfa252e7e7a10fbe9bb3
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-09 02:01:30 -0500

    transaction form

--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -1,7 +1,22 @@
+import "./TransactionForm.css";
+
 export default function TransactionForm() {
-    return (
-        <form>
-            <p>form element</p>
-        </form>
-    )
+  return (
+    <form>
+      <label htmlFor="item-name">Item Name: </label>
+      <input type="text" id="item-name" />
+      <label htmlFor="amount">Amount: </label>
+      <input type="number" id="amount" />
+      <label htmlFor="from">From: </label>
+      <input type="tex" id="from" />
+      <label htmlFor="category">Category: </label>
+      <select id="category">
+        <option value="">choose a category</option>
+        <option value="">Income</option>
+        <option value="">Entertainment Expense</option>
+      </select>
+      <label htmlFor="date">Date: </label>
+      <input type="date" id="date" />
+    </form>
+  );
 }
