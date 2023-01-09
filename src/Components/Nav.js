b95cff4fcacedadf9b580a4ed342b42c872390fd
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-09 14:45:31 -0500

    home icon css

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-import HomePageIconLight from "../Assets/home-page-icon-light.png";
+import HomeIconLight from "../Assets/home-icon-light.png";
+import HomeIconHover from "../Assets/home-icon-hover.png";
 import "./Nav.css";
 
 export default function Nav() {
   return (
     <nav>
       <Link to="/">
-        <img src={HomePageIconLight} alt="home page icon" />
+        <div id="home-icon">
+          <img id="home-icon-light" src={HomeIconLight} alt="home page icon" />
+          <img id="home-icon-hover" src={HomeIconHover} alt="home page icon" />
+        </div>
       </Link>
       <Link to="/transactions">Budget App Index</Link>
       <Link id="link-new" to="/transactions/new">
