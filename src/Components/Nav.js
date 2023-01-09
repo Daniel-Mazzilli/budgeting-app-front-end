b2caeceb60c30ada585bd1a3fd0cc82351538362
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-09 11:37:48 -0500

    working on crud functionality and css

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import HomePageIcon from "../Assets/home-page-icon.png";
+import HomePageIconLight from "../Assets/home-page-icon-light.png";
 import "./Nav.css";
 
 export default function Nav() {
   return (
     <nav>
       <Link to="/">
-        <img src={HomePageIcon} alt="home page icon" />
+        <img src={HomePageIconLight} alt="home page icon" />
       </Link>
       <Link to="/transactions">Budget App Index</Link>
       <Link id="link-new" to="/transactions/new">
--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -1,22 +1,74 @@
+import { useState } from "react";
 import "./TransactionForm.css";
 
 export default function TransactionForm() {
+  const [transaction, setTransaction] = useState({
+    itemName: "",
+    amount: 0,
+    from: "",
+    category: "",
+    date: "",
+  });
+
+  const handleChange = (event) => {
+    let val = event.target.value;
+    if (event.target.id === "amount" && val) {
+      val = +val;
+    }
+    setTransaction({ ...transaction, [event.target.id]: val });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
-      <label htmlFor="item-name">Item Name: </label>
-      <input type="text" id="item-name" />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="itemName">Item Name: </label>
+      <input
+        id="itemName"
+        type="text"
+        value={transaction.itemName}
+        onChange={handleChange}
+        required
+      />
       <label htmlFor="amount">Amount: </label>
-      <input type="number" id="amount" />
+      <input
+        id="amount"
+        type="number"
+        value={transaction.amount}
+        onChange={handleChange}
+        required
+      />
       <label htmlFor="from">From: </label>
-      <input type="tex" id="from" />
+      <input
+        id="from"
+        type="tex"
+        value={transaction.from}
+        onChange={handleChange}
+        required
+      />
       <label htmlFor="category">Category: </label>
-      <select id="category">
+      <select
+        id="category"
+        value={transaction.category}
+        onChange={handleChange}
+        required
+      >
         <option value="">choose a category</option>
-        <option value="">Income</option>
-        <option value="">Entertainment Expense</option>
+        <option value="Income">Income</option>
+        <option value="Entertainment Expense">Entertainment Expense</option>
+        <option value="Exercise Expense">Exercise Expense</option>
       </select>
       <label htmlFor="date">Date: </label>
-      <input type="date" id="date" />
+      <input
+        id="date"
+        type="date"
+        value={transaction.date}
+        onChange={handleChange}
+        required
+      />
+      <input type="submit" id="submit" value="SUBMIT" />
     </form>
   );
 }
