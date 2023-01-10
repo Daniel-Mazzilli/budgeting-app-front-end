433e4d9330a3d19591b9acc0f830c670039a8b97
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-01-10 16:11:53 -0500

    improving form

--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { ContextData } from "../Provider/Provider";
 import axios from "axios";
 import "./TransactionForm.css";
@@ -38,6 +38,26 @@ export default function TransactionForm() {
     setTransaction({ ...transaction, [event.target.id]: val });
   };
 
+  const handleReset = () => {
+    if (id !== undefined) {
+      axios
+        .get(`${API}/transactions/${id}`)
+        .then((res) => setTransaction(res.data))
+        .catch((err) => {
+          console.log(err);
+          navigate("/not-found");
+        });
+    } else {
+      setTransaction({
+        item_name: "",
+        amount: 0,
+        from: "",
+        category: "",
+        date: "",
+      });
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -70,62 +90,66 @@ export default function TransactionForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="item_name">Name: </label>
-      <input
-        id="item_name"
-        type="text"
-        value={transaction.item_name}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="amount">Amount: </label>
-      <input
-        id="amount"
-        type="number"
-        value={transaction.amount}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="from">From: </label>
-      <input
-        id="from"
-        type="tex"
-        value={transaction.from}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="category">Category: </label>
-      <select
-        id="category"
-        value={transaction.category}
-        onChange={handleChange}
-        required
-      >
-        <option value=""></option>
-        <option value="Deposit">Deposit</option>
-        <option value="Expense">Expense</option>
-        <option value="Income">Income</option>
-        <option value="Profit">Profit</option>
-        <option value="Purchase">Purchase</option>
-        <option value="Subscription">Subscription</option>
-        <option value="Taxes">Taxes</option>
-        <option value="Travel and Leisure">Travel and Leisure</option>
-        <option value="Utility Bills">Utility Bills</option>
-      </select>
-      <label htmlFor="date">Date: </label>
-      <input
-        id="date"
-        type="date"
-        value={transaction.date}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="submit"
-        id="submit"
-        value={id ? "SUBMIT CHANGES" : "CREATE NEW ITEM"}
-      />
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="item_name">Name: </label>
+        <input
+          id="item_name"
+          type="text"
+          value={transaction.item_name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="amount">Amount: </label>
+        <input
+          id="amount"
+          type="number"
+          value={transaction.amount}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="from">From: </label>
+        <input
+          id="from"
+          type="tex"
+          value={transaction.from}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="category">Category: </label>
+        <select
+          id="category"
+          value={transaction.category}
+          onChange={handleChange}
+          required
+        >
+          <option value=""></option>
+          <option value="Deposit">Deposit</option>
+          <option value="Expense">Expense</option>
+          <option value="Income">Income</option>
+          <option value="Profit">Profit</option>
+          <option value="Purchase">Purchase</option>
+          <option value="Subscription">Subscription</option>
+          <option value="Taxes">Taxes</option>
+          <option value="Travel and Leisure">Travel and Leisure</option>
+          <option value="Utility Bills">Utility Bills</option>
+        </select>
+        <label htmlFor="date">Date: </label>
+        <input
+          id="date"
+          type="date"
+          value={transaction.date}
+          onChange={handleChange}
+          required
+        />
+        <p onClick={handleReset}>reset</p>
+        <input
+          type="submit"
+          id="submit"
+          value={id ? "SUBMIT CHANGES" : "CREATE NEW ITEM"}
+        />
+      </form>
+      {id && <Link to={`/transactions/${id}`}>Back</Link>}
+    </>
   );
 }
